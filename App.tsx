17bc96606cd5dfe85517f7fa29a16eae19e88b6c
Violet <violet@newworldinc.io>
2022-10-15 16:42:48 -0400

    tailwind working on react native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeWindStyleSheet } from 'nativewind';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
+  NativeWindStyleSheet.setOutput({
+    default: 'native'
+  })
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
